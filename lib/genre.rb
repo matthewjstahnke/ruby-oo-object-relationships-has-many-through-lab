f457515defa29b76ab74b34a9c55f253122e7db5
matthewjstahnke <matthewjstahnke@gmail.com>
2020-11-28 20:30:36 -0600

    commit tree ede3672c1e662f10366ab8467a52c3b5fea3514a

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,30 @@
+class Genre
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def songs
+        Song.all.select{|song| song.genre == self}
+    end
+
+    def artists
+        songs.map(&:artist)
+    end
+
+    def add_song(song)
+        songs << song
+    end
+end
+
+
+
 
